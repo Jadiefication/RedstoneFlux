1fb6dc88a1bc8e85a2c768aa626ab9def7556ade
Traqueur-dev <traqueur.dev@gmail.com>
2024-10-07 18:05:00 +0200

    fix (test-plugin) : update test plugin

--- test-plugin/src/main/java/fr/traqueur/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/fr/traqueur/testplugin/TestPlugin.java
@@ -1,17 +1,30 @@
 package fr.traqueur.testplugin;
 
-import fr.traqueur.testplugin.tests.EnergyTest;
+import fr.traqueur.commands.api.CommandManager;
+import fr.traqueur.energylib.api.items.ItemsFactory;
+import fr.traqueur.energylib.api.types.MechanicType;
+import fr.traqueur.testplugin.tests.*;
+import fr.traqueur.testplugin.tests.commands.EnergyCommand;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        new EnergyTest(this);
+        CommandManager commandManager = new CommandManager(this);
+
+        commandManager.registerConverter(MechanicType.class, "component-type", new ComponentsTypeConverter());
+
+        commandManager.registerCommand(new EnergyCommand(this));
+
+        ItemsFactory.registerItem(Material.FURNACE, BlockProducer.class);
+        ItemsFactory.registerItem(Material.DISPENSER, BlockConsumer.class);
+        ItemsFactory.registerItem(Material.CHEST, BlockStorage.class);
+        ItemsFactory.registerItem(Material.WHITE_STAINED_GLASS, BlockTransporter.class);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
