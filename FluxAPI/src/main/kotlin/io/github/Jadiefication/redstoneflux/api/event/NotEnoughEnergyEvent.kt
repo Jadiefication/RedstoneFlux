4cea903a8602786051d91be51bcc51c70a54b0b5
Jadiefication <jadeczjade008@gmail.com>
2025-07-17 21:24:47 +0200

    chore: documentation

--- FluxAPI/src/main/kotlin/io/github/Jadiefication/redstoneflux/api/event/NotEnoughEnergyEvent.kt
+++ FluxAPI/src/main/kotlin/io/github/Jadiefication/redstoneflux/api/event/NotEnoughEnergyEvent.kt
@@ -4,6 +4,12 @@ import io.github.Jadiefication.redstoneflux.api.components.EnergyComponent
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
+/**
+ * Event which happens when a machine doesn't have enough energy.
+ * @param requiredEnergy the energy amount required.
+ * @param givenEnergy the energy amount said machine needs.
+ * @param consumer the machine consuming said energy.
+ */
 class NotEnoughEnergyEvent(
     val requiredEnergy: Double,
     val givenEnergy: Double,
