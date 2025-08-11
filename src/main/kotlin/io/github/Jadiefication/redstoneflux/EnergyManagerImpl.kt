51961b726df666c66591c381423396f8c815f9b2
Jadiefication <jadeczjade008@gmail.com>
2025-08-11 16:32:29 +0200

    feat: abstract energy manager for more types of managers

--- src/main/kotlin/io/github/Jadiefication/redstoneflux/EnergyManagerImpl.kt
+++ src/main/kotlin/io/github/Jadiefication/redstoneflux/EnergyManagerImpl.kt
@@ -4,8 +4,11 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import io.github.Jadiefication.redstoneflux.api.EnergyAPI
 import io.github.Jadiefication.redstoneflux.api.EnergyManager
+import io.github.Jadiefication.redstoneflux.api.components.BaseNetwork
 import io.github.Jadiefication.redstoneflux.api.components.EnergyComponent
+import io.github.Jadiefication.redstoneflux.api.components.EnergyComponentBuilder
 import io.github.Jadiefication.redstoneflux.api.components.EnergyNetwork
+import io.github.Jadiefication.redstoneflux.api.components.ItemComponentBuilder
 import io.github.Jadiefication.redstoneflux.api.exceptions.SameEnergyTypeException
 import io.github.Jadiefication.redstoneflux.api.items.ItemsFactory
 import io.github.Jadiefication.redstoneflux.api.mechanics.EnergyMechanic
@@ -60,6 +63,11 @@ class EnergyManagerImpl(
     private var updaterTask: Job? = null
 
     /**
+     * Items builder for energy components.
+     */
+    private val builder: EnergyComponentBuilder
+
+    /**
      * Create a new EnergyManagerImpl instance.
      *
      * @param energyLib the EnergyLib instance
@@ -68,6 +76,13 @@ class EnergyManagerImpl(
         this.gson = this.createGson()
         ItemsFactory.gson = this.gson
         this.networks = HashSet<EnergyNetwork>()
+
+        builder = EnergyComponentBuilder(
+            gson,
+            energyLib.energyTypeKey,
+            energyLib.mechanicClassKey,
+            energyLib.mechanicKey
+        )
     }
 
     /**
@@ -124,7 +139,7 @@ class EnergyManagerImpl(
 
         location.block.type = Material.AIR
         if (player.gameMode != GameMode.CREATIVE) {
-            val result = this.createItemComponent(energyType!!, mechanicType, component)
+            val result = this.createItemComponent(component, builder)
             player.world.dropItemNaturally(location, result)
         }
 
@@ -228,36 +243,11 @@ class EnergyManagerImpl(
     /**
      * {@inheritDoc}
      */
-    override fun createItemComponent(
-        type: EnergyType,
-        mechanicType: MechanicType,
-        mechanic: EnergyComponent<*>
+    override fun <T : ItemComponentBuilder<EnergyComponent<*>>> createItemComponent(
+        component: EnergyComponent<*>,
+        builder: T
     ): ItemStack {
-        require(mechanic.javaClass.isAssignableFrom(mechanicType.clazz)) { "Mechanic type " + mechanicType.clazz + " is not compatible with mechanic " + mechanic.mechanic?.javaClass }
-
-        val item: ItemStack = ItemsFactory.getItem(mechanic)
-            .orElseThrow(Supplier { IllegalArgumentException("Item not found for mechanic " + mechanic.javaClass) })
-
-        val meta: ItemMeta? = item.itemMeta
-        requireNotNull(meta) { "ItemMeta is null!" }
-        val persistentDataContainer: PersistentDataContainer = meta.persistentDataContainer
-        persistentDataContainer.set(
-            energyLib.energyTypeKey,
-            EnergyTypePersistentDataType.INSTANCE,
-            type
-        )
-        persistentDataContainer.set(
-            energyLib.mechanicClassKey,
-            PersistentDataType.STRING,
-            mechanic.javaClass.getName()
-        )
-        persistentDataContainer.set(
-            energyLib.mechanicKey,
-            PersistentDataType.STRING,
-            this.gson.toJson(mechanic, mechanic.javaClass)
-        )
-        item.setItemMeta(meta)
-        return item
+        return (builder as EnergyComponentBuilder).buildItem(component)
     }
 
     /**
@@ -280,9 +270,9 @@ class EnergyManagerImpl(
         this.updaterTask!!.cancel()
     }
 
-    override fun deleteNetwork(network: EnergyNetwork) {
+    override fun <T : BaseNetwork<EnergyComponent<*>>> deleteNetwork(network: T) {
         network.delete()
-        this.networks.remove(network)
+        this.networks.remove(network as EnergyNetwork)
     }
 
     /**
