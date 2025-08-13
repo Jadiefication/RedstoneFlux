dd5f11a5bea09f2495201fa9be5b8ca6027465b6
Jadiefication <jadeczjade008@gmail.com>
2025-08-13 17:14:57 +0200

    chore: deprecate EnergyTransporter

--- FluxAPI/src/main/kotlin/io/github/Jadiefication/redstoneflux/api/mechanics/EnergyTransporter.kt
+++ FluxAPI/src/main/kotlin/io/github/Jadiefication/redstoneflux/api/mechanics/EnergyTransporter.kt
@@ -3,4 +3,9 @@ package io.github.Jadiefication.redstoneflux.api.mechanics
 /**
  * This interface is used to represent an energy transporter.
  */
+@Deprecated(
+    message = "Deprecated since 1.3.3",
+    replaceWith = ReplaceWith("Transporter"),
+    level = DeprecationLevel.WARNING
+)
 interface EnergyTransporter : EnergyMechanic
